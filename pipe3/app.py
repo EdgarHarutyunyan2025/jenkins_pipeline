0705a89d6e87080bea2b6bd3ab54dbad1073a2db
edgar <edgar_harutyunyan_2025@mail.ru>
2024-12-11 23:22:26 +0400

    build docker image

--- pipe3/app.py
+++ pipe3/app.py
@@ -1,26 +1,28 @@
-"""Example of a clean Python script with perfect pylint score"""
+import requests
 
-def greet(name: str) -> str:
-    if not name:
-        raise ValueError("Name cannot be empty")
-    return f"Hello, {name}!"
+def weather_forecast(city):
 
-
-def calculate_sum(a: int, b: int) -> int:
-    return a + b
+    api_key='2ad207f1576cc623c5396f3df877c5d7'
+    response=requests.get('https://api.openweathermap.org/data/2.5/weather',params={'q':city,'units':'metric','APPID':api_key})
 
 
-def main() -> None:
-    """Main function to demonstrate greeting and sum calculation."""
-    try:
-        name = "Alice"
-        print(greet(name))
-        result = calculate_sum(5, 3)
-        print(f"The sum is: {result}")
-    except ValueError as e:
-        print(f"Error: {e}")
+    if response.status_code ==200:
+        data=response.json()
+        try:
+            print('City',data['name'])
+            for i in data['main']:
+                print('  ',i,'-->',data['main'][i])
+        except Exception as error_name:
+            print(error_name)
 
 
-if __name__ == "__main__":
+    else:
+        print('status_code--',response.status_code)
+
+
+def main():
+    city_name="GYUMRI"
+    weather_forecast(city_name)
+
+if __name__=='__main__':
     main()
-
